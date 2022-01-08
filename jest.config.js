8ee66a0adef07bd7f2310039ed227a8df23cbfa5
Tiago Jose <tiago_jvo@outlook.com>
2022-01-08 18:14:32 -0300

    refactor: update all project dependencies and configuration files

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,27 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+    collectCoverageFrom: [
+      '<rootDir>/src/**/*.ts',
+      '!<rootDir>/src/main/**',
+      '!<rootDir>/src/**/index.ts'
+    ],
+    coverageDirectory: 'coverage',
+    coverageProvider: 'babel',
+    moduleNameMapper: {
+      '@/tests/(.+)': '<rootDir>/tests/$1',
+      '@/(.+)': '<rootDir>/src/$1'
+    },
+    testMatch: ['**/*.spec.ts'],
+    roots: [
+      '<rootDir>/src',
+      '<rootDir>/tests'
+    ],
+    transform: {
+      '\\.ts$': 'ts-jest'
+    },
+    clearMocks: true
+  }
