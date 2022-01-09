1f7b220add7ec9c28ee098797a164572afb77f35
Tiago Jose <tiago_jvo@outlook.com>
2022-01-09 01:32:29 -0300

    feat: ensure ValidatorsComposite returns undefined if all validators passes

--- tests/application/validators/composite.spec.ts
+++ tests/application/validators/composite.spec.ts
@@ -26,4 +26,15 @@ describe('ValidatorsComposite', () => {
 
     expect(error).toEqual(new RequiredFieldError('any_field'))
   })
+
+  it('should return undefined if all validators passes', () => {
+    const firstValidator = new RequiredFieldValidator('any_field', 'any_value')
+    const secondValidator = new RequiredFieldValidator('any_field_2', 'any_value')
+
+    const sut = new ValidatorsComposite([firstValidator, secondValidator])
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
 })
