5345877ca833a5df4c5a007bf662600f0ee46b39
Tiago Jose <tiago_jvo@outlook.com>
2022-01-09 02:36:19 -0300

    feat: add typeOrm connection with a real postgres database

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,5 +3,8 @@ import './config/module-alias'
 
 import { app } from './config/app'
 import { config } from './config/env'
+import { createConnection } from 'typeorm'
 
-app.listen(config.port, () => console.log(`Server running on port : ${config.port}`))
+createConnection().then(() => {
+  app.listen(config.port, () => console.log(`Server running on port : ${config.port}`))
+}).catch(console.error)
