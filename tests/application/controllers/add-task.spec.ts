cc21d458191193445b23e61191a42b4af95dbb09
Tiago Jose <tiago_jvo@outlook.com>
2022-01-08 21:02:52 -0300

    feat: ensure AddTaskController returns 401 if description is not defined in request

--- tests/application/controllers/add-task.spec.ts
+++ tests/application/controllers/add-task.spec.ts
@@ -3,6 +3,7 @@ type HttpRequest = {
   description: string
   isComplete: boolean
   isFavorite: boolean
+  [key: string]: string | boolean
 }
 
 type HttpResponse = {
@@ -12,6 +13,19 @@ type HttpResponse = {
 
 class AddTaskController {
   async handle (httpRequest: Partial<HttpRequest>): Promise<HttpResponse> {
+    const requiredFields = ['title', 'description']
+
+    for (const requiredField of requiredFields) {
+      if (!httpRequest[requiredField]) {
+        return {
+          status: 401,
+          data: {
+            error: `Field ${requiredField} is required`
+          }
+        }
+      }
+    }
+
     return {
       status: 401,
       data: {
@@ -24,11 +38,22 @@ describe('AddTaskController', () => {
   it('should return 401 if task title is not defined in request', async () => {
     const sut = new AddTaskController()
 
-    const result = await sut.handle({ title: '', description: 'any_description', isComplete: false, isFavorite: false })
+    const result = await sut.handle({ description: 'any_description', isComplete: false, isFavorite: false })
 
     expect(result).toEqual({
       status: 401,
       data: { error: 'Field title is required' }
+    })
+  })
+
+  it('should return 401 if task description is not defined in request', async () => {
+    const sut = new AddTaskController()
+
+    const result = await sut.handle({ title: 'any_title', isComplete: false, isFavorite: false })
+
+    expect(result).toEqual({
+      status: 401,
+      data: { error: 'Field description is required' }
     })
   })
 })
