effee21a5f8683f641c2eb14a317420d6089f289
Tiago Jose <tiago_jvo@outlook.com>
2022-01-09 02:32:47 -0300

    feat: create and apply ExpressRouterAdapter

--- src/main/routes/add-task.ts
+++ src/main/routes/add-task.ts
@@ -1,7 +1,12 @@
+import { AddTaskController } from '@/application/controllers'
+import { AddTaskService } from '@/data/services'
+import { PgTaskRepository } from '@/infra/database/postgres/repositories'
+import { adaptExpressRoute } from '@/infra/http/express-router'
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.get('/', (request, response) => {
-    response.send({ message: 'Its workingg' })
-  })
+  const pgTaskRepository = new PgTaskRepository()
+  const addTaskService = new AddTaskService(pgTaskRepository)
+  const controller = new AddTaskController(addTaskService)
+  router.post('/tasks', adaptExpressRoute(controller))
 }
